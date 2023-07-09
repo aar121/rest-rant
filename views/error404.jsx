f7d813a0eedc532be1049d644f8c1f260f9e754d
aar121 <arifrahim12345@gmail.com>
2023-07-09 14:01:34 -0400

    Adding Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -1,15 +1,24 @@
-const React = require('react')
-const Def = require('./default')
+const React = require("react");
+const Def = require("./default");
 
-function error404 () {
-    return (
-      <Def>
-          <main>
-              <h1>404: PAGE NOT FOUND</h1>
-              <p>Oops, sorry, we can't find this page!</p>
-          </main>
-      </Def>
-    )
-  }
+function error404() {
+  return (
+    <Def>
+      <main>
+        <h1>404: PAGE NOT FOUND</h1>
+        <div>
+          <img src="/images/skeletonscratchhead.jpg" alt="Skeleton"></img>
+          <div>
+            Photo by <a href="https://www.masterfile.com/">Masterfile</a> on
+            <a href="https://www.masterfile.com/image/en/640-01360160">
+              Masterfile
+            </a>
+          </div>
+        </div>
+        <p>Oops, sorry, we can't find this page!</p>
+      </main>
+    </Def>
+  );
+}
 
-  module.exports = error404
+module.exports = error404;
--- views/home.jsx
+++ views/home.jsx
@@ -1,17 +1,28 @@
-const React = require('react')
-const Def = require('./default')
+const React = require("react");
+const Def = require("./default");
 
-function home () {
-    return (
-      <Def>
-          <main>
-              <h1>HOME</h1>
-          </main>
-      </Def>
-    )
-  }
-<a href="/places">
-  <button className="btn-primary">Places Page</button>
-</a>
+function home() {
+  return (
+    <Def>
+      <main>
+        <h1>REST-Rant</h1>
+        <div>
+          <img src="/images/burger.jpg" alt="Burger"></img>
+          <div>
+            Photo by{" "}
+            <a href="https://photo.thepicturepantry.com/-/galleries/photographer/alanna-taylor-tobin">
+              Alanna Taylor-Tobin
+            </a>{" "}
+            on{" "}
+            <a href="https://www.thepicturepantry.com/">The Picture Pantry</a>
+          </div>
+        </div>
+        <a href="/places">
+          <button className="btn-primary">Places Page</button>
+        </a>
+      </main>
+    </Def>
+  );
+}
 
-module.exports = home
+module.exports = home;
