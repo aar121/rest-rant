89e9305ccf3ef6e83db4beb8bef5b37c635190d5
aar121 <arifrahim12345@gmail.com>
2023-07-10 13:21:37 -0400

    Adding and rendering a new view

--- controllers/places.js
+++ controllers/places.js
@@ -2,6 +2,11 @@ const router = require("express").Router();
 
 // More code here in a moment
 
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+
+
 router.get("/", (req, res) => {
   let places = [
     {
