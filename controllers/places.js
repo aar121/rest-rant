bfd9f8a0de12abd15ceb6029043b288bfa248ff0
aar121 <arifrahim12345@gmail.com>
2023-07-11 14:09:18 -0400

    minor changes

--- controllers/places.js
+++ controllers/places.js
@@ -1,34 +1,34 @@
+const express = require("express");
 const router = require("express").Router();
-
+const places = require("../models/places.js");
 // More code here in a moment
-
-router.get('/new', (req, res) => {
-  res.render('places/new')
-})
-
-router.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('POST /places')
-})
-
+require("dotenv").config();
 
 router.get("/", (req, res) => {
-  let places = [
-    {
-      name: "H-Thai-ML",
-      city: "Seattle",
-      state: "WA",
-      cuisines: "Thai, Pan-Asian",
-      pic: "/images/thairestaurant.jpg",
-    },
-    {
-      name: "Coding Cat Cafe",
-      city: "Phoenix",
-      state: "AZ",
-      cuisines: "Coffee, Bakery",
-      pic: "/images/cafe.jpg",
-    },
-  ];
+  res.render("places/index", { places });
+});
+
+router.post("/", (req, res) => {
+  //console.log(req.body);
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = "http://placekitten.com/400/400";
+  }
+  if (!req.body.city) {
+    req.body.city = "Anytown";
+  }
+  if (!req.body.state) {
+    req.body.state = "USA";
+  }
+  places.push(req.body);
+  res.redirect("/places");
+});
+
+router.get("/new", (req, res) => {
+  res.render("places/new");
+});
+
+router.get("/", (req, res) => {
   res.render("places/index", { places });
 });
 
