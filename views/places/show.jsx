2244ba683f3459850cca119beaee5b9e9ad419c3
aar121 <arifrahim12345@gmail.com>
2023-07-11 21:26:31 -0400

    updated edit and delete route, functionality needs some work

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,14 +1,33 @@
-const React = require('react')
-const Def = require('../default')
+const React = require("react");
+const Def = require("../default");
 
-function show () {
-    return (
-        <Def>
-          <main>
-            <h1>Show Page</h1>
-          </main>
-        </Def>
-    )
+function show(data) {
+  return (
+    <Def>
+      <main>
+        <div className="place-1">
+          <h1>{data.place.name}</h1>
+          <h2>Rating</h2>
+          <p>Not Rated</p>
+          <p className="text-center">{data.place.cuisines}</p>
+          <img src={data.place.pic} alt={data.place.name} />
+          <p className="text-center">
+            Located in {data.place.city}, {data.place.state}
+          </p>
+          <h2>Comments</h2>
+          <p>No comments yet</p>
+          <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+            Edit
+          </a>
+          <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+            <button type="submit" className="btn btn-danger">
+              Delete
+            </button>
+          </form>
+        </div>
+      </main>
+    </Def>
+  );
 }
 
-module.exports = show
+module.exports = show;
