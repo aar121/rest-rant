503d651670b817a6d2db99fb00cd075fae014d5a
aar121 <arifrahim12345@gmail.com>
2023-07-16 14:17:10 -0400

    changed paths in places controller and refactored code

--- controllers/places.js
+++ controllers/places.js
@@ -1,10 +1,74 @@
 const express = require("express");
 const router = require("express").Router();
-const places = require("../models/places.js");
+const db = require('../models')
+//const places = require("../models/places.js");
 // More code here in a moment
 require("dotenv").config();
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
+  db.Place.find()
+  .then((places) => {
+    res.render('places/index', { places })
+  })
+  .catch(err => {
+    console.log(err)
+    res.render('error404')
+  })
+})
+
+router.post('/', (req, res) => {
+  db.Place.create(req.body)
+  .then(() => {
+      res.redirect('/places')
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
+})
+
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+
+router.get('/:id', (req, res) => {
+  db.Place.findById(req.params.id)
+  .then(place => {
+      res.render('places/show', { place })
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
+})
+
+
+router.put('/:id', (req, res) => {
+  res.send('PUT /places/:id stub')
+})
+
+router.delete('/:id', (req, res) => {
+  res.send('DELETE /places/:id stub')
+})
+
+router.get('/:id/edit', (req, res) => {
+  res.send('GET edit form stub')
+})
+
+router.post('/:id/rant', (req, res) => {
+  res.send('GET /places/:id/rant stub')
+})
+
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send('GET /places/:id/rant/:rantId stub')
+})
+
+module.exports = router
+
+
+
+
+/*/* router.get("/", (req, res) => {
   res.render("places/index", { places });
 });
 
@@ -60,6 +124,7 @@ router.delete('/:id', (req, res) => {
 })
 
 router.get('/:id/edit', (req, res) => {
+  console.log(req.params.id)
   let id = Number(req.params.id)
   if (isNaN(id)) {
       res.render('error404')
@@ -68,7 +133,7 @@ router.get('/:id/edit', (req, res) => {
       res.render('error404')
   }
   else {
-    res.render('places/edit', { place: places[id] })
+    res.render('places/edit', { place: places[id], id:id })
   }
 })
 
@@ -99,4 +164,5 @@ router.put('/:id', (req, res) => {
   }
 })
 
-module.exports = router;
+module.exports = router
+/**/
