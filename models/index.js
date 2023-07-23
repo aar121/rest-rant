baf377e072fab3bfc2210e4af15b5202069e282d
aar121 <arifrahim12345@gmail.com>
2023-07-23 09:26:27 -0400

    comments

--- models/index.js
+++ models/index.js
@@ -6,4 +6,5 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true
 })
 
+module.exports.Comment = require('./comments')
 module.exports.Place = require('./places')
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,21 @@ const React = require("react");
 const Def = require("../default");
 
 function show(data) {
+  let comments = <h3 className="inactive">No comments yet!</h3>;
+  if (data.place.comments.length) {
+    comments = data.place.comments.map((c) => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? "Rant! ðŸ˜¡" : "Rave! ðŸ˜»"}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <stong>- {c.author}</stong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      );
+    });
+  }
   return (
     <Def>
       <main>
@@ -16,8 +31,8 @@ function show(data) {
           </h3>
           <div className="col-sm-6">
             <h2>Description</h2>
-            <h3>{data.place.showEstablished() }</h3>
-            <h4>Serving { data.place.cuisines}</h4>
+            <h3>{data.place.showEstablished()}</h3>
+            <h4>Serving {data.place.cuisines}</h4>
           </div>
           <h2>Comments</h2>
           <p>No comments yet</p>
@@ -30,6 +45,52 @@ function show(data) {
             </button>
           </form>
         </div>
+        <form method="POST" action={`/places/${data.place.id}/comment`}>
+    <div>
+      <label htmlFor="author">Author</label>
+      <input
+        className="form"
+        id="author"
+        name="author"
+        placeholder="your name"
+      ></input>
+    </div>
+    <div>
+      <label htmlFor="content">Content</label>
+      <input
+        className="form"
+        id="content"
+        name="content"
+        placeholder="your message"
+      ></input>
+    </div>
+    <div>
+      <label htmlFor="star rating">Star Rating</label>
+      <input
+        className="form"
+        id="rating"
+        name="rating"
+        type="range"
+        min="0"
+        max="5"
+        step="0.5"
+      ></input>
+    </div>
+    <div>
+      <label htmlFor="checkbox">Rant</label>
+      <input
+        className="checkbox"
+        type="checkbox"
+        value="rant"
+        id="rant"
+        name="rant"
+      ></input>
+    </div>
+    <div>
+      <button className="button" type="submit"></button>
+      Add Your Comment
+    </div>
+  </form>;
       </main>
     </Def>
   );
